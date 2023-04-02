7bbfd5a07374048062826ee092cf9c2e7b7955d9
Lauren McDonald <mcdonl9@mcmaster.ca>
2023-04-02 17:33:54 -0400

    Co-authored-by: Ayushi1972 <Ayushi1972@users.noreply.github.com>

--- cabTap/src/RequestRideSharePage.java
+++ cabTap/src/RequestRideSharePage.java
@@ -1,5 +1,22 @@
+import java.time.*;
 
 public class RequestRideSharePage {
     private String arrivalTime;
+    private String dropOff;
+    private String pickip;
+    private LocalDate date;
+    private LocalTime time;
+    private int passengerNumber;
+    //private RoadMap map;
+    private int cost;
 
+    private void validateTripInfo(){
+        
+    }
+
+    private void displayOpenRides(){
+
+    }
+    
+    
 }
