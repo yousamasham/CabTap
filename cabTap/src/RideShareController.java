264e54d33a9300c0085ccbcd8ef84f974d040b58
Cjeran <cierdieb649ed.amdsb.ca>
2023-04-03 23:56:56 -0400

    Skeleton of RideShareController and TripChanges

---
+++ cabTap/src/RideShareController.java
@@ -0,0 +1,18 @@
+public class RideShareController {
+
+    private String[] riders;
+
+    protected void requestRideShare(){}
+
+    protected void requestRide() {}
+
+    protected void requestGameStatus() {}
+
+    protected void presentFare() {}
+
+    protected void rateRider() {}
+
+    protected void displayRideStatus() {}
+
+    protected void calculateRideFare() {}
+}
