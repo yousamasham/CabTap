fb1a67d5b3daea4cb8cd9da52e4203aac6a8611c
Yousam Asham <45107433+yousamasham@users.noreply.github.com>
2023-04-04 20:12:42 -0400

    Add new proj

---
+++ CabTap/app/src/main/java/com/example/cabtap/LoginController.java
@@ -0,0 +1,30 @@
+package com.example.cabtap;
+
+import static android.text.TextUtils.isEmpty;
+
+import android.widget.EditText;
+
+public class LoginController {
+
+    public boolean checkUsername(EditText user, EditText pass) {
+
+        CharSequence username = user.getText().toString();
+        boolean isValid = true;
+        if (isEmpty(username)) {
+            user.setError("You must enter username to login!");
+            isValid = false;
+        }
+
+        CharSequence password = pass.getText().toString();
+        if (isEmpty(password)) {
+            pass.setError("You must enter password to login!");
+            isValid = false;
+        }
+
+//        if (isValid) {
+            //TODO
+//        }
+        return false;
+    }
+
+}
---
+++ CabTap/app/src/main/java/com/example/cabtap/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.cabtap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    private ViewPager2 viewPager;
+    AuthenticationPagerAdapter pagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = findViewById(R.id.pager);
+
+        pagerAdapter = new AuthenticationPagerAdapter(this);
+        // add Fragments in your ViewPagerFragmentAdapter class         myAdapter.addFragment(new first_fragment());
+        pagerAdapter.addFragment(new LoginPage());
+        pagerAdapter.addFragment(new RegistrationPage());
+
+        viewPager.setAdapter(pagerAdapter);
+    }
+
+    private class AuthenticationPagerAdapter extends FragmentStateAdapter {
+
+        private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+        public AuthenticationPagerAdapter( FragmentActivity fa) {
+            super(fa);
+        }
+
+        @Override
+        public Fragment createFragment(int position) {
+            return fragmentList.get(position);
+        }
+        public void addFragment(Fragment fragment) {
+            fragmentList.add(fragment);
+        }
+        @Override
+        public int getItemCount() {
+            return fragmentList.size();
+        }
+    }
+
+}
