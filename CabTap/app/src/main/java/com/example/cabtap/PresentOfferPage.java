47571d72c813f89c8f41856637e7130e14c96326
Ayushi Amin <amina23@mcmaster.ca>
2023-04-06 19:57:52 -0400

    first push

--- CabTap/app/src/main/java/com/example/cabtap/PresentOfferPage.java
+++ CabTap/app/src/main/java/com/example/cabtap/PresentOfferPage.java
@@ -2,7 +2,7 @@ package com.example.cabtap;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -16,32 +16,18 @@ public class PresentOfferPage extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.fragment_presentofferpage);
+        setContentView(R.layout.activity_presentofferpage);
         recyclerView.findViewById(R.id.recyclerView);
         recyclerAdapter = new RecyclerAdapter(requestedRides);
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(recyclerAdapter);
 
+        // now adds lines to seperate each item
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration((dividerItemDecoration));
+
         // get list of available requests from dispatcher and store in the requestedRides list
-    }
-
-    //private void dispalyRequestRides(){
-    //ArrayList<TripInformation> rides = DispatcherController.getRides();
-    //display all options, create an accept and reject button for each
-    //if (isNull(rides)){
-    // show message saying no offers yet
-    // }
-    //}
-
-    // called when accept button is pressed
-    private void acceptRide(){
-        // calls dispatcher and dispatcher pairs riders together
-    }
-
-    // called when reject button is pressed
-    private void rejectRide(){
-        // dispatcher removes from list of requests for the rider.
     }
 
 }
