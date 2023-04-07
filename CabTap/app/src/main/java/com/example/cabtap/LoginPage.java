239c3123ea55bd4505288b47a85216dd5622bbd7
Jason Tran <jjsontran@gmail.com>
2023-04-07 12:18:27 -0400

    WIP Validating Login/Registration

--- CabTap/app/src/main/java/com/example/cabtap/LoginPage.java
+++ CabTap/app/src/main/java/com/example/cabtap/LoginPage.java
@@ -1,5 +1,6 @@
 package com.example.cabtap;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -12,22 +13,38 @@ import android.widget.*;
 
 public class LoginPage extends Fragment {
 
-    EditText username;
+    EditText userName;
     EditText password;
     Button login;
+    LoginController controller;
 
 
     @Override
     public void onViewCreated(View view,  Bundle savedInstanceState) {
-        username = (EditText) getView().findViewById(R.id.et_username);
+        userName = (EditText) getView().findViewById(R.id.et_username);
         password = (EditText) getView().findViewById(R.id.et_password);
         login = (Button) getView().findViewById(R.id.btn_login);
+        controller = new LoginController();
 
         //sendLoginCredentials
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO CONNECT WITH BACKEND TO VALIDATE
+                try{
+                    SessionDetails session = new SessionDetails(controller.login(userName,  password));
+                    Intent intent = new Intent(getActivity(), ProfileActivity.class);
+                    intent.putExtra("legalName", session.getSessionLegalName());
+                    intent.putExtra("username", session.getSessionUsername());
+                    intent.putExtra("phoneNumber", session.getSessionPhoneNumber());
+                    startActivity(intent);
+                }
+                catch(Exception E){
+                    try {
+                        throw E;
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                }
             }
         });
 
