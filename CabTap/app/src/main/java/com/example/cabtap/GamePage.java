08a3d670799e8d56b5c89a3b6b79f9b40340fa2e
Jason Tran <jjsontran@gmail.com>
2023-04-09 18:54:46 -0400

    initial commit

---
+++ CabTap/app/src/main/java/com/example/cabtap/GamePage.java
@@ -0,0 +1,14 @@
+package com.example.cabtap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GamePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_page);
+    }
+}
